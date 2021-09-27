508d832e454210b9ca3fbc2042f900e96a92b46b
Yasin Erkul <yasinerkull@outlook.dk>
2021-09-27 20:21:53 +0200

    Add project files.

---
+++ RestService/Controllers/HttpController.cs
@@ -0,0 +1,56 @@
+﻿using FootballPLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace RestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class HttpController : Controller
+    {
+        private readonly Manager manager = new Manager();
+
+        // Get All
+        [HttpGet]
+        public IEnumerable<FootballPlayer> Get()
+        {
+            return manager.GetAll();
+        }
+
+        // Get by id
+        [HttpGet("{id}")]
+        public FootballPlayer GetById(int id)
+        {
+            return manager.GetByID(id);
+        }
+
+
+        // POST
+        [HttpPost]
+        public FootballPlayer Post([FromBody] FootballPlayer value)
+        {
+            return manager.Add(value);
+        }
+
+        // PUT
+        [HttpPut("{id}")]
+        public FootballPlayer Put(int id, [FromBody] FootballPlayer value)
+        {
+            return manager.Update(id, value);
+        }
+
+        // DELETE 
+        [HttpDelete("{id}")]
+        public FootballPlayer Delete(int id)
+        {
+            return manager.Delete(id);
+        }
+    }
+}
+
---
+++ RestService/Manager/Manager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+using FootballPLayer;
+
+namespace RestService
+{
+    public class Manager
+    {
+       
+        private static int NextId = 1;
+
+        public readonly static List<FootballPlayer> FP = new List<FootballPlayer>()
+        {
+            new FootballPlayer(NextId++, "Christiano Ronaldo", 750, 7),
+            new FootballPlayer(NextId++, "Lionel Messi", 750, 31),
+            new FootballPlayer(NextId++, "Andre Silva", 750, 11)
+        };
+
+        public List<FootballPlayer> GetAll()
+        {
+            return new List<FootballPlayer>(FP);
+        }
+
+        public FootballPlayer GetByID(int id)
+        {
+            return FP.Find(fp => fp.Id == id);
+        }
+
+   
+        public FootballPlayer Add(FootballPlayer NewPlayer)
+        {
+            NewPlayer.Id = NextId++;
+            FP.Add(NewPlayer);
+            return NewPlayer;
+        }
+
+        public FootballPlayer Update(int id, FootballPlayer Updates)
+        {
+            FootballPlayer player = FP.Find(FP => FP.Id == id);
+            if (player == null)
+            {
+                return null;
+            }
+            player.Id = Updates.Id;
+            player.Name = Updates.Name;
+            player.Price = Updates.Price;
+            player.ShirtNumber = Updates.ShirtNumber;
+
+            return player;
+        }
+
+        public FootballPlayer Delete(int id)
+        {
+            FootballPlayer fp = FP.Find(FP => FP.Id == id);
+            if (fp == null)
+            {
+                return null;
+            }
+            FP.Remove(fp);
+            return fp;
+        }
+    }
+}
